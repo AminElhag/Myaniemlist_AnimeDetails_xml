817a44fa660c6f1c920497fbd21629fe5e98e8bc
amin jalal <aminjalalzoom@gmail.com>
2019-12-13 13:02:04 +0200

    complet

---
+++ app/src/main/java/com/kafwapteam/nogamenolifeshowdetails/MainActivity.kt
@@ -0,0 +1,13 @@
+/**Don't track myanimelist just a simulation**/
+package com.kafwapteam.nogamenolifeshowdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
